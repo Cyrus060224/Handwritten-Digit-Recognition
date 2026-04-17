da7b9d07806560cdb3048b0c0832528899f40dca
Cyrus Wang <wxcyrus@qq.com>
2026-04-18 00:38:32 +0800

    chore: 彻底移除 build 文件夹并添加 .gitignore

--- build/CMakeFiles/NoobNetwork.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for NoobNetwork.
-# This may be replaced when dependencies are built.
--- build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/cyrus/Desktop/CrossPlatform_NN/raylib
-# Build directory: /Users/cyrus/Desktop/CrossPlatform_NN/build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
